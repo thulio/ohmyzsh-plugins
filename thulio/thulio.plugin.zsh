696a94dfb3154a75ae2c135ede2ed9025a94e47e
Thúlio Costa <thulio@thuliocosta.com>
2014-03-16 19:22:35 -0300

    Adding start-mysql alias

--- thulio/thulio.plugin.zsh
+++ thulio/thulio.plugin.zsh
@@ -236,6 +236,7 @@ EOF
 alias json='python -mjson.tool | pygmentize -f terminal256 -l javascript -O style=native'
 alias start-redis="redis-server /usr/local/etc/redis.conf"
 alias start-mongodb="mongod run --config /usr/local/Cellar/mongodb/$(mongo --version | cut -d":" -f 2 | cut -d " " -f 2)/mongod.conf"
+alias start-mysql="sudo /usr/bin/mysqld_safe --datadir='/var/lib/mysql'"
 alias pinstall="pip install -M"
 alias ssh="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=120"
 alias ll='ls -lh'
