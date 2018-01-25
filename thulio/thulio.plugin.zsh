46b027b11d7689d02b50a5bf19a1b1dceeb4277c
Thúlio Costa <thulio@thuliocosta.com>
2018-01-25 17:21:00 -0200

    Fix mix_format_modified

--- thulio/thulio.plugin.zsh
+++ thulio/thulio.plugin.zsh
@@ -278,7 +278,7 @@ if [ "$(uname -s)" = "Linux" ] ; then
 fi
 
 alias mix_format="ASDF_ELIXIR_VERSION=1.6.0 mix format"
-alias mix_format_modified="(git clean --dry-run | awk '{print $3;}' && git ls-files -m) | ASDF_ELIXIR_VERSION=1.6.0 xargs mix format"
+alias mix_format_modified="(git clean --dry-run | awk '{print $3;}' && git ls-files -m) | egrep '.ex|.exs|.eex' | ASDF_ELIXIR_VERSION=1.6.0 xargs mix format"
 
 export ERL_AFLAGS="-kernel shell_history enabled"
 
