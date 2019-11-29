99eb45efb124d782a3d1fc40f7810d90d3cb4d2f
Thúlio Costa <thulio@thuliocosta.com>
2019-11-29 20:56:30 -0300

    chore: Simplify uuid function

--- thulio/thulio.plugin.zsh
+++ thulio/thulio.plugin.zsh
@@ -270,27 +270,7 @@ function fix_elixir_umbrella {
 }
 
 function uuid() {
-    # Usage: uuid
-    C="89ab"
-
-    for ((N=0;N<16;++N)); do
-        B="$((RANDOM%256))"
-
-        case "$N" in
-            6)  printf '4%x' "$((B%16))" ;;
-            8)  printf '%c%x' "${C:$RANDOM%${#C}:1}" "$((B%16))" ;;
-
-            3|5|7|9)
-                printf '%02x-' "$B"
-            ;;
-
-            *)
-                printf '%02x' "$B"
-            ;;
-        esac
-    done
-
-    printf '\n'
+    uuidgen | awk '{print tolower($0)}'
 }
 
 function render_md() {
